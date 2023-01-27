5921cce54f4c94dc31ab98394770b350c7a3ffd9
IaroslavLasiichuk <lasmant@yahoo.com>
2023-01-27 10:52:15 -0700

    Message disapears

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,24 +3,40 @@ $(function () {
   // This code uses the id in the containing time-block as a key to save the user input in local storage
   $('.btn').on("click", function () {
     let idOfparent = $(this).parent();
-    let message = $('.appoitment');
+    // let message = $('.appoitment');
     idOfparent.each(function (i) {
       let id = $(this).prop('id');
       localStorage.setItem(id, $(idOfparent).children('textarea').val());
-      $(message).text('Added to localStorage✅');
+      // $(message).text('Added to localStorage✅');
     });
+  });
+  let timerCount = 5;
+
+  // Timer for disappear message
+  $('.btn').on("click", function messageDisapear () {
+    const timerInterval = setInterval(() => {
+      timerCount--;
+      let message = $('.appoitment');
+      console.log(timerCount);
+      $(message).text('Added to localStorage✅');
+      if (timerCount === 0) {
+        clearInterval(timerInterval);
+        timerCount = 5;
+        $(message).text('');
+      }
+  }, 1000);
   });
 
   //This code to apply the past, present, or future class to each time
   //  block by comparing the id to the current hour and
   // get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements
-  const currentHour = `${dayjs().format('H')}`;
+  const currentHour = `${'hour-'}${dayjs().format('HH')}`;
   let blocks = $('.time-block');
-  blocks.each(function (i, element) {
-    let divEl = $(element);
-    let divIdis = $(this).prop('id');
-    let textAreaEl = $(element).children('textarea');
+  for (let i = 0; i < blocks.length; i++){
+    let divEl = $(blocks[i]);
+    let divIdis = $(divEl).prop("id");
+    let textAreaEl = $(blocks[i]).children('textarea');
     for (let i = 0; i < localStorage.length; i++) {
       if (localStorage.key(i) === divIdis) {
         textAreaEl.val(localStorage.getItem(localStorage.key(i)));
@@ -35,8 +51,31 @@ $(function () {
     if (divIdis > currentHour) {
       divEl.addClass('future');
     }
-  });
+  }
+
+
+  // blocks.each(function (i, element) {
+  //   let divEl = $(element);
+  //   let divIdis = $(this).prop('id');
+  
+  //   let textAreaEl = $(element).children('textarea');
+  //   for (let i = 0; i < localStorage.length; i++) {
+  //     if (localStorage.key(i) === divIdis) {
+  //       textAreaEl.val(localStorage.getItem(localStorage.key(i)));
+  //     }
+  //   }
+  //   if (divIdis == currentHour) {
+  //     divEl.addClass('present');
+  //   }
+  //   if (divIdis < currentHour) {
+  //     divEl.addClass('past');
+  //   }
+  //   if (divIdis > currentHour) {
+  //     divEl.addClass('future');
+  //     console.log(currentHour);
+  //   }
+  // });
 
   // Displays the current date in the header of the page
-  $('#currentDay').text(dayjs().format('dddd, MMMM, YYYY, h:m a'));
+  $('#currentDay').text(dayjs().format('dddd, MMMM, YYYY, HH:mm a'));
 });
